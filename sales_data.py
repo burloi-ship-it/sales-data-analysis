b06e55fba19ab99b9bedbe9f9cd2e60c2b6ef62b
Razvan Burloi <burloi@protonmail.com>
2026-02-28 14:04:54 +0200

    Added sales_data list structure describe in sales_data.py

--- sales_data.py
+++ sales_data.py
@@ -1,4 +1,4 @@
 # sales_data.py
-
+# Weekly sales data: [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]
 sales_data = [230, 200, 310, 290, 400, 150, 180]
 print(f"Sales data for week: {sales_data}.)
